c0676cddbf4cbc769f960c306f0b9334cb1b8c6e
biaguadanhin <beatriz.8857@colegioprogresso.g12.br>
2026-03-04 09:31:10 -0300

    comecamos a estilizar

---
+++ app.js
@@ -0,0 +1,14 @@
+const numero1 = document.getElementById("nota1")
+const numero2 = document.getElementById("nota2")
+const button = document.getElementById("button")
+const resultado = document.getElementById("resultado")
+
+function calculaMedia(){
+  const nota1 = Number(numero1.value)
+  const nota2 = Number(numero2.value)
+
+ const media = (nota1+nota2)/2
+ resultado.innerText = `A MÉDIA É ${media.toFixed(4)}`
+}
+
+button.addEventListener("click", calculaMedia)
